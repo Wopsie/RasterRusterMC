82a9d322c51398ad1cb88528c67f86884934fe68
Jochem <joaghim.beltman@gmail.com>
2022-02-03 14:41:21 +0100

    fill triangle render through keybinding. implementing wireframe render

--- Hello_Triangle/src/main.rs
+++ Hello_Triangle/src/main.rs
@@ -7,32 +7,37 @@ const WIDTH: usize = 500;
 const HEIGHT: usize = 500;
 
 //actually just cross product? or determinant?
-pub fn edge_function(v0: Vec2, v1: Vec2, p: Vec2) -> f32 
-{
+pub fn edge_function(v0: Vec2, v1: Vec2, p: Vec2) -> f32 {
     (p.x - v0.x) * (v1.y - v0.y) - (p.y - v0.y) * (v1.x - v0.x)
 }
 
-pub fn index_to_coords(p: usize, height: usize, width : usize) -> (usize, usize) 
+pub fn bresenham_function(v0: Vec2, v1: Vec2, p: Vec2) -> f32
 {
+    0.0
+}
+
+pub fn index_to_coords(p: usize, height: usize, width: usize) -> (usize, usize) {
     (p % height, p / width)
 }
 
 //convert colors to u32
-pub fn to_argb8(a: u8, r: u8, g: u8, b: u8) -> u32 
-{
+pub fn to_argb8(a: u8, r: u8, g: u8, b: u8) -> u32 {
     let mut argb: u32 = a as u32; //a
     argb = (argb << 8) + r as u32; //r
     argb = (argb << 8) + g as u32; //g
     argb = (argb << 8) + b as u32; //b
-    argb    //returns argb. Syntax funkyness
-    //you can do "shadowing" on the data(?)
-    //by default statics cannot be mutable
-    //same for constants
-    //need to use "unsafe" for that
+    argb //returns argb. Syntax funkyness
+         //you can do "shadowing" on the data(?)
+         //by default statics cannot be mutable
+         //same for constants
+         //need to use "unsafe" for that
 }
 
 fn main() {
     let mut buffer: Vec<u32> = vec![to_argb8(255, 255, 0, 0); WIDTH * HEIGHT];
+
+    let mut redTriangle: bool = false;
+    let mut wireFrameRend: bool = false;
 
     let mut window = Window::new(
         "Test - ESC to exit",
@@ -44,70 +49,103 @@ fn main() {
         panic!("{}", e);
     });
 
-
     let triangle = [
         glam::vec2(100.0, 100.0),
         glam::vec2(250.0, 400.0),
         glam::vec2(400.0, 100.0),
     ];
 
-    let edge = (glam::vec2(0.0, 0.0), Vec2::new(WIDTH as f32, HEIGHT as f32));
+    //let edge = (glam::vec2(0.0, 0.0), Vec2::new(WIDTH as f32, HEIGHT as f32));
 
-    
-    for i in 0..buffer.len() 
-    {
-        let coords = index_to_coords(i, HEIGHT, WIDTH);
-        let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
-        let m0 = edge_function(coords, triangle[0], triangle[2]);
-        let m1 = edge_function(coords, triangle[2], triangle[0]);
-        let m2 = edge_function(coords, triangle[0], triangle[1]);
-        
-        buffer[i] = to_argb8(
-            255, 
-            (m2 * 255.0) as u8,
-            (m0 * 255.0) as u8,
-            (m1 * 255.0) as u8,
-        );
+    // for i in 0..buffer.len()
+    // {
+    //     let coords = index_to_coords(i, HEIGHT, WIDTH);
+    //     let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
+    //     let m0 = edge_function(coords, triangle[0], triangle[2]);
+    //     let m1 = edge_function(coords, triangle[2], triangle[0]);
+    //     let m2 = edge_function(coords, triangle[0], triangle[1]);
 
-        // if m0 >= 0.0 && m1 >= 0.0 && m2 >= 0.0 {
-        //     buffer[i] = to_argb8(255, 0, 255, 0);
-        // } else {
-        //     buffer[i] = to_argb8(0, 0, 0, 0);
-        // }
-    }
-    
-    let mut count : usize = 0;
+    //     buffer[i] = to_argb8(
+    //         255,
+    //         (m2 * 255.0) as u8,
+    //         (m0 * 255.0) as u8,
+    //         (m1 * 255.0) as u8,
+    //     );
+
+    //     // if m0 >= 0.0 && m1 >= 0.0 && m2 >= 0.0 {
+    //     //     buffer[i] = to_argb8(255, 0, 255, 0);
+    //     // } else {
+    //     //     buffer[i] = to_argb8(0, 0, 0, 0);
+    //     // }
+    // }
+
+    let mut count: usize = 0;
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    while window.is_open() && !window.is_key_down(Key::Escape) 
-    {
+    while window.is_open() && !window.is_key_down(Key::Escape) {
         for i in buffer.iter_mut() {
-            *i = 0; // write something more funny here!
+            *i = 0; //screen clear
 
             let coords = index_to_coords(count, HEIGHT, WIDTH);
-            count+=1;
+            count += 1;
             let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
 
-            let m0 = edge_function(coords, triangle[0], triangle[1]);
-            let m1 = edge_function(coords, triangle[1], triangle[2]);
-            let m2 = edge_function(coords, triangle[2], triangle[0]);
+            let mut m0 : f32 = 0.0;
+            let mut m1 : f32 = 0.0;
+            let mut m2 : f32 = 0.0;
+            
+            if !wireFrameRend
+            {
+                m0 = edge_function(triangle[0], triangle[1], coords);
+                m1 = edge_function(triangle[1], triangle[2], coords);
+                m2 = edge_function(triangle[2], triangle[0], coords);
+            } else {
+                //do bresenham here
+                m0 = bresenham_function(triangle[0], triangle[1], coords);
+                m1 = bresenham_function(triangle[1], triangle[2], coords);
+                m2 = bresenham_function(triangle[2], triangle[0], coords);
+            }
+            
 
-            *i = to_argb8(
-                255, 
-                (m2 * 255.0) as u8,
-                (m0 * 255.0) as u8,
-                (m1 * 255.0) as u8,
-            );   
+            if !redTriangle {
+                *i = to_argb8(
+                    255,
+                    (m2 * 255.0) as u8,
+                    (m0 * 255.0) as u8,
+                    (m1 * 255.0) as u8,
+                );
+            } else if m0 >= 0.0 && m1 >= 0.0 && m2 >= 0.0 {
+                *i = to_argb8(255, 255, 0, 0);
+            }
 
+            if wireFrameRend {
+                // render with bresenham algorithm
+            }
 
             // if(side >= 0.0)
             // {
-            //     *i = to_argb8(255, 255, 0, 0);   
+            //     *i = to_argb8(255, 255, 0, 0);
             // }else {
             //     *i = to_argb8(255, 0, 255, 0);
             // }
         }
+
+        if window.is_key_down(Key::Space) {
+            redTriangle = true;
+        } else if window.is_key_down(Key::W) {
+            wireFrameRend = true;
+        } else {
+            redTriangle = false;
+            wireFrameRend = false;
+        }
+
+        // if window.is_key_down(Key::W)
+        // {
+        //     wireFrameRend = true;
+        // } else {
+        //     wireFrameRend = false;
+        // }
 
         count = 0;
 
